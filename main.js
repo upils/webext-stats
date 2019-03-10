4813ab3a52f79d433d99b90ec6d8fc5f46d5235f
paul <upils@eodras.com>
2019-03-10 18:22:39 +0100

    Add export to xlsx

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const downloadFile = require('./utils')
 const unzip = require("unzip-crx");
 const fs = require('fs');
 const rmdir = require('rimraf');
+const xlsx = require('node-xlsx');
 
 const baseUrlList = 'https://chrome.google.com/webstore/ajax/item?'
 
@@ -11,7 +12,7 @@ const params = {
 'gl': 'FR',
 'pv': '20181009',
 'mce': 'atf%2Cpii%2Crtr%2Crlb%2Cgtc%2Chcn%2Csvp%2Cwtd%2Cnrp%2Chap%2Cnma%2Cnsp%2Cdpb%2Cc3d%2Cncr%2Cctm%2Cac%2Chot%2Cmac%2Cfcf%2Crma',
-'count': '100',
+'count': '10',
 'marquee': 'true',
 'category': 'collection%2Fnew_noteworthy_extensions',
 'sortBy': '0',
@@ -69,12 +70,10 @@ if (typeof Array.prototype.reIndexOf === 'undefined') {
 // Main function
 (async () => {
 try {
-    console.log(urlList)
+    let data = [['Name', 'ID', 'Permissions', 'Update URL', 'CSP']]
     const response = await getData(urlList)
-    console.log(response)
     const cleanResponse = JSON.parse(response.split("\n").slice(1).join("\n"))[0][1]
     const extensions = cleanResponse[1]
-    console.log(extensions)
     for (let i = 0; i < extensions.length; i++ ) {
         const extensionId = extensions[i][0]
         const name = extensions[i][1]
@@ -87,10 +86,26 @@ try {
             await unzip(filePath)
             let manifestRaw = fs.readFileSync('./extensions/' + extensionId + '/manifest.json');  
             let manifest = JSON.parse(manifestRaw);
+            
             console.log(name)
+        
+            let permissions = []
             if (manifest.permissions) {
-                console.log(manifest.permissions)
+                permissions = manifest.permissions
             }
+            let updateUrl
+            if (manifest.update_url) {
+                updateUrl = manifest.update_url
+            }
+            let contentSecurityPolicy
+            if (manifest.content_security_policy) {
+                contentSecurityPolicy = manifest.content_security_policy
+                console.log(contentSecurityPolicy)
+            }
+            const extensionArray = [name, extensionId, permissions, updateUrl, contentSecurityPolicy]
+            data.push(extensionArray);
+
+            // Cleaning
             rmdir('./extensions/' + extensionId + '/', (err) => {
                 if (err) throw err;
             })
@@ -101,6 +116,12 @@ try {
             console.log(error)
         }
     }
+    const resultsBuffer = xlsx.build([{name: "Results", data: data}]);
+    fs.writeFile('results.xlsx', resultsBuffer, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+
 } catch (error) {
     console.log(error)
 }
